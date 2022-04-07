278eeaeea3e2b4f1347bfa0c536df25c7b9d3bcd
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-04-06 23:26:35 -0300

    Creating products

--- src/http/graphql/resolvers/products.resolver.ts
+++ src/http/graphql/resolvers/products.resolver.ts
@@ -1,9 +1,10 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
 import {AuthorizationGuard} from "../../auth/authorization.guard"
 import { ProductsService } from '../../../services/products.service';
 
+import { CreateProductInput } from '../inputs/create-product-input';
 import {Product} from "../models/product"
 
 @Resolver()
@@ -11,8 +12,13 @@ export class ProductsResolver {
     constructor(private productsService: ProductsService){}
 
     @Query(() => [Product])
-    // @UseGuards(AuthorizationGuard)
     products() {
         return this.productsService.listAllProducts()
+    }
+
+    @UseGuards(AuthorizationGuard)
+    @Mutation(() => Product)
+    createProduct(@Args('data') data: CreateProductInput){
+        return this.productsService.createProduct(data)
     }
 }
