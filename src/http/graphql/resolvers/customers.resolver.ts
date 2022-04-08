8b804d4800b9c325d59d8af4969db79a1ea1c07f
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-04-07 23:55:13 -0300

    listing with details

--- src/http/graphql/resolvers/customers.resolver.ts
+++ src/http/graphql/resolvers/customers.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ResolveField, Parent } from '@nestjs/graphql';
 
 import {AuthorizationGuard} from "../../auth/authorization.guard"
 import { ProductsService } from '../../../services/products.service';
@@ -9,14 +9,23 @@ import {Product} from "../models/product"
 import { Customer } from '../models/customer';
 import { CustomersService } from 'src/services/customers.service';
 import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { PurchasesService } from 'src/services/purchases.service';
 
 @Resolver(() => Customer)
 export class CustomersResolver {
-    constructor(private customerService: CustomersService){}
+    constructor(
+        private customerService: CustomersService,
+        private purchasesService: PurchasesService
+    ){}
 
     @UseGuards(AuthorizationGuard)
     @Query(() => Customer)
     me(@CurrentUser() user: AuthUser){
         return this.customerService.getCustomerByAuthUserid(user.sub)
+    }
+
+    @ResolveField()
+    purchases(@Parent() customer: Customer){
+        return this.purchasesService.listAllFromCustomer(customer.id)
     }
 }
